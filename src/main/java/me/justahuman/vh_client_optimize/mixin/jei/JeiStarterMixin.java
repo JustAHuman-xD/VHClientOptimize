655017a3aa3b0972492a707e9141f1057e16d7dd
JustAHuman-xD <imjustahuman.xd@gmail.com>
2025-08-04 14:35:46 -0500

    1.0.1

---
+++ src/main/java/me/justahuman/vh_client_optimize/mixin/jei/JeiStarterMixin.java
@@ -0,0 +1,19 @@
+package me.justahuman.vh_client_optimize.mixin.jei;
+
+import me.justahuman.vh_client_optimize.VHClientOptimize;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.runtime.IJeiRuntime;
+import mezz.jei.startup.JeiStarter;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = JeiStarter.class, remap = false)
+public class JeiStarterMixin {
+    @Redirect(method = "lambda$start$0", at = @At(value = "INVOKE", target = "Lmezz/jei/api/IModPlugin;onRuntimeAvailable(Lmezz/jei/api/runtime/IJeiRuntime;)V"))
+    private static void handleRuntimeSync(IModPlugin instance, IJeiRuntime jeiRuntime) {
+        Minecraft minecraft = Minecraft.getInstance();
+        VHClientOptimize.JEI_FUTURE.thenRun(() -> minecraft.execute(() -> instance.onRuntimeAvailable(jeiRuntime)));
+    }
+}
